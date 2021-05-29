887b8fac117e265c2f044b03468d2b62751efae7
Italo Fernandes <italo.feernandes@gmail.com>
2021-05-29 11:11:19 -0300

    Criada classe Carro and fix Test

---
+++ src/main/java/br/com/impacta/models/Carro.java
@@ -0,0 +1,33 @@
+package br.com.impacta.models;
+
+public class Carro {
+
+    private String cor;
+    private Integer ano;
+    private Double valor;
+
+    public String getCor() {
+        return this.cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public Integer getAno() {
+        return this.ano;
+    }
+
+    public void setAno(Integer ano) {
+        this.ano = ano;
+    }
+
+    public Double getValor() {
+        return this.valor;
+    }
+
+    public void setValor(Double valor) {
+        this.valor = valor;
+    }
+
+}
--- src/test/java/br/com/impacta/lab/JavaSpringTemplateApplicationTests.java
+++ src/test/java/br/com/impacta/lab/JavaSpringTemplateApplicationTests.java
@@ -2,22 +2,21 @@ package br.com.impacta.lab;
 
 import static org.junit.Assert.assertEquals;
 
-import java.util.ArrayList;
 import br.com.impacta.models.*;
 import org.junit.Test;
 
-class JavaSpringTemplateApplicationTests {
+public class JavaSpringTemplateApplicationTests {
 
 	@Test
 	public void shouldHaveCarwithEncapsulatePrinciple() {
-		
+
 		Carro carro = new Carro();
 		carro.setCor("Azul");
 		carro.setAno(2021);
 		carro.setValor(50000.00);
-		
+
 		assertEquals("Azul", carro.getCor());
-		
+
 	}
-	
+
 }
